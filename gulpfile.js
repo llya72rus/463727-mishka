5ced563f89c66ab361ba809fd58bba1ac4821223
llya72rus <32940323+llya72rus@users.noreply.github.com>
2018-02-11 20:16:54 +0300

    Выполнил задание №8

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,14 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
 var server = require("browser-sync").create();
-var webp = require('gulp-webp');
-
-gulp.task('webp', () =>
-    gulp.src('source/img/*.jpg')
-        .pipe(webp())
-        .pipe(gulp.dest('webp-images'))
-);
+var webp = require("gulp-webp");
+var del = require("del");
+var run = require("run-sequence");
 
 gulp.task("style", function() {
   gulp.src("source/sass/style.scss")
@@ -21,13 +21,67 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("images", function() {
+  gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"))
+});
+
+gulp.task("webp", function() {
+  gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"))
+});
+
+gulp.task("sprite", function() {
+    gulp.src([
+      "source/img/icon-academy.svg",
+      "source/img/logo-footer.svg",
+      "source/img/icon-cart.svg",
+      "source/img/icon-search.svg",
+      "source/img/icon-fb.svg",
+      "source/img/icon-insta.svg",
+      "source/img/icon-tw.svg",
+      "source/img/icon-phone.svg",
+      "source/img/icon-mail.svg",
+      "source/img/icon-left-arrow.svg",
+      "source/img/icon-right-arrow.svg"
+    ])
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"))
+});
+
+gulp.task("copy", function() {
+  gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.html"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  del("build");
+});
+
+gulp.task("serve",  function() {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -36,4 +90,14 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("build", function(done) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "sprite",
+    done
+  );
 });
--- source/js/main.js
+++ source/js/main.js
@@ -16,7 +16,6 @@
         navMain.classList.add('main-nav--closed');
       }
     });
-
   }
 
   var modal = document.querySelector('.modal');
@@ -34,4 +33,33 @@
       modal.classList.remove('modal--show');
     });
   }
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (modal.classList.contains("modal--show")) {
+        modal.classList.remove("modal--show");
+      }
+    }
+  });
 })();
+
+
+// This function has to be in global scope in order to initialize map from HTML
+function initMap() {
+  var academy = {lat: 59.938614, lng: 30.323032},
+      mapNode = document.getElementById('google-map');
+
+  if (mapNode) {
+    var map = new google.maps.Map(mapNode, {
+      zoom: 16,
+      center: {lat: 59.9392, lng: 30.323032},
+      scrollwheel: false
+    });
+
+    var marker = new google.maps.Marker({
+      position: academy,
+      map: map,
+      icon: "../img/map-pin.svg"
+    });
+  }
+}
